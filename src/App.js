c0b1768d54b290a90fe42c683ea201b73ff78f5b
LG Ultra <jhy0285@gmail.com>
2022-07-05 18:06:21 +0900

    개못함 ㅠㅠ

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Main from "./pages/main/Main";
 import Test from "./components/test/Test";
 function App() {
